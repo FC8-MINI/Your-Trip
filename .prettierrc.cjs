6cb198b0eb46cfdd23abeeaad3c1d9037a32c348
catrt <rt990122@gmail.com>
2024-06-18 15:37:08 +0900

    Initial: 프로젝트 개발 환경 세팅

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+const baseConfig = require("@marshallku/prettier-config");
+
+module.exports = {
+  ...baseConfig,
+  tabWidth: 2,
+};
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,165 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Thin.woff2") format("woff2"),
+      url("/fonts/Pretendard-Thin.woff") format("woff");
+    font-weight: 100;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-ExtraLight.woff2") format("woff2"),
+      url("/fonts/Pretendard-ExtraLight.woff") format("woff");
+    font-weight: 200;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Light.woff2") format("woff2"),
+      url("/fonts/Pretendard-Light.woff") format("woff");
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Regular.woff2") format("woff2"),
+      url("/fonts/Pretendard-Regular.woff") format("woff");
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Medium.woff2") format("woff2"),
+      url("/fonts/Pretendard-Medium.woff") format("woff");
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-SemiBold.woff2") format("woff2"),
+      url("/fonts/Pretendard-SemiBold.woff") format("woff");
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Bold.woff2") format("woff2"),
+      url("/fonts/Pretendard-Bold.woff") format("woff");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-ExtraBold.woff2") format("woff2"),
+      url("/fonts/Pretendard-ExtraBold.woff") format("woff");
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Pretendard";
+    src:
+      url("/fonts/Pretendard-Black.woff2") format("woff2"),
+      url("/fonts/Pretendard-Black.woff") format("woff");
+    font-weight: 900;
+    font-style: normal;
+  }
+
+  :root {
+    --color-primary: #3b82f6;
+    --color-primary-hover: #3269c3;
+    --color-gray: #e0e2e7;
+    --color-gray-hover: #f7f8fa;
+    --color-black: #333;
+    --color-white: #fff;
+    --color-error: #ee5151;
+
+    font-size: 10px;
+  }
+
+  * {
+      margin: 0;
+      padding: 0;
+      font-family: "Pretendard", "Helvetica", "Arial", sans-serif;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+  }
+  
+  body {
+    min-width: 32rem;
+    font-family: "Pretendard", "Helvetica", "Arial", sans-serif;
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 1.25;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    scroll-behavior: smooth
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    padding: 0;
+    border: 0;
+    background: transparent;
+    appearance: none;
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  button,
+  input,
+  select,
+  textarea {
+    font-size: 1.6rem;
+    background-color: transparent;
+    border: 0;
+    border-radius: 0.6rem;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+  input::-webkit-inner-spin-button {
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+  }
+`;
+
+export default GlobalStyle;
