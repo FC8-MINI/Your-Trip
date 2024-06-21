1b61beebe34d073602a9623f1e4d1f555d6040aa
catrt <rt990122@gmail.com>
2024-06-21 18:58:25 +0900

    Design: Header 컴포넌트 그림자 삭제

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -11,9 +11,6 @@ export const HeaderStyled = styled.header`
   background-color: var(--color-white);
 
   border-bottom: 1px solid var(--color-gray);
-  box-shadow:
-    rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 `;
 
 export const HeaderContainer = styled(ContainerStyled)`
