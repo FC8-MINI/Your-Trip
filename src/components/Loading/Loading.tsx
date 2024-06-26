699eacc9a0fc535c10a554860e19e2511d18d3d6
minnug <parkminnug@gmail.com>
2024-06-26 17:05:39 +0900

    Feat: 로딩 컴포넌트 추가

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import { LoadingLayout, SpinnerBox } from "./Loading.styles";
+
+const Loading = () => {
+  return (
+    <LoadingLayout>
+      <SpinnerBox />
+    </LoadingLayout>
+  );
+};
+
+export default Loading;
