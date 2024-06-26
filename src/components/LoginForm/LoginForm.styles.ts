5eb6e36cdd44ed011b6e5b33e385e46d4bdbdef2
catrt <rt990122@gmail.com>
2024-06-26 14:30:36 +0900

    Design: 로그인, 회원가입 제출폼 media query 추가 및 Input, Button 컴포넌트 media query 추가

--- src/components/LoginForm/LoginForm.styles.ts
+++ src/components/LoginForm/LoginForm.styles.ts
@@ -11,6 +11,9 @@ export const LoginFormStyled = styled.form`
   justify-content: center;
   align-items: center;
   gap: 1.6rem;
+
+  width: 100%;
+  max-width: 40rem;
 `;
 
 export const OAuthText = styled.span`
