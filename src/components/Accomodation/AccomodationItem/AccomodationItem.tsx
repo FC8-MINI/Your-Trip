891f66220199e7c84d97cb887f12bb0ff845eebb
catrt <rt990122@gmail.com>
2024-06-27 11:41:24 +0900

    Design: AccomodationItem 디자인 스타일 변경

--- src/components/Accomodation/AccomodationItem/AccomodationItem.tsx
+++ src/components/Accomodation/AccomodationItem/AccomodationItem.tsx
@@ -1,10 +1,12 @@
 import {
+  AccomodationItemBox,
   AccomodationItemImageBox,
   AccomodationItemStyled,
   Description,
   DescriptionAddress,
   DescriptionAvailable,
-  DescriptionCategory,
+  DescriptionDetail,
+  DescriptionDetailText,
   DescriptionPrice,
   DescriptionTitle,
 } from "./AccomodationItem.styles";
@@ -16,34 +18,38 @@ import { AccomodationItemProps } from "./AccomodationItem.types";
 import Image from "next/image";
 
 const AccomodationItem = ({ accomodationItem }: AccomodationItemProps) => {
-  const { id, categoryId, name, address, parkingAvailable, cookingAvailable } = accomodationItem;
+  const { id, name, address, parkingAvailable, cookingAvailable, description } = accomodationItem;
 
   return (
     <li>
       <Link href={`/accomodation/${id}`}>
-        <AccomodationItemStyled>
-          <AccomodationItemImageBox>
-            <Image src="/images/hero_background_image.jpg" alt={name} fill objectFit="cover" />
-          </AccomodationItemImageBox>
-          <Description>
-            <DescriptionCategory>{categoryId}</DescriptionCategory>
-            <DescriptionTitle>{name}</DescriptionTitle>
-            <DescriptionAddress>{address}</DescriptionAddress>
-            <DescriptionAvailable>
-              {parkingAvailable ? (
-                <TbParking style={{ color: "#A8A8A8" }} />
-              ) : (
-                <TbParkingOff style={{ color: "#f57373" }} />
-              )}
-              {cookingAvailable ? (
-                <GiMeal style={{ color: "#A8A8A8" }} />
-              ) : (
-                <MdOutlineNoMeals style={{ color: "#f57373" }} />
-              )}
-            </DescriptionAvailable>
-            <DescriptionPrice>123,000원</DescriptionPrice>
-          </Description>
-        </AccomodationItemStyled>
+        <AccomodationItemBox>
+          <AccomodationItemStyled>
+            <AccomodationItemImageBox className="accomodation-description__image">
+              <Image src="/images/hero_background_image.jpg" alt={name} fill objectFit="cover" />
+            </AccomodationItemImageBox>
+            <Description>
+              <DescriptionTitle>{name}</DescriptionTitle>
+              <DescriptionAddress>{address}</DescriptionAddress>
+              <DescriptionAvailable>
+                {parkingAvailable ? (
+                  <TbParking style={{ color: "#A8A8A8" }} />
+                ) : (
+                  <TbParkingOff style={{ color: "#f57373" }} />
+                )}
+                {cookingAvailable ? (
+                  <GiMeal style={{ color: "#A8A8A8" }} />
+                ) : (
+                  <MdOutlineNoMeals style={{ color: "#f57373" }} />
+                )}
+              </DescriptionAvailable>
+              <DescriptionPrice>123,000원</DescriptionPrice>
+              <DescriptionDetail className="accomodation-description__detail">
+                <DescriptionDetailText>{description}</DescriptionDetailText>
+              </DescriptionDetail>
+            </Description>
+          </AccomodationItemStyled>
+        </AccomodationItemBox>
       </Link>
     </li>
   );
