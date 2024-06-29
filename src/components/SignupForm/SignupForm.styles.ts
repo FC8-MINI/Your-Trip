d131ae978ffe29e739c8725c46568258dd66e377
catrt <rt990122@gmail.com>
2024-06-29 14:48:40 +0900

    Refactor: 회원가입 제출폼 media query 간단하게 수정

--- src/components/SignupForm/SignupForm.styles.ts
+++ src/components/SignupForm/SignupForm.styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { ContainerStyled } from "../Container";
-import { device } from "@/styles/media";
 
 export const SignupFormContainer = styled(ContainerStyled)`
   padding-top: 4rem;
@@ -13,10 +12,8 @@ export const SignupFormStyled = styled.form`
   align-items: center;
   gap: 1.6rem;
 
-  @media ${device.mobile} {
-    width: 100%;
-    max-width: 40rem;
-  }
+  width: 100%;
+  max-width: 40rem;
 `;
 
 export const ToLoginText = styled.p`
