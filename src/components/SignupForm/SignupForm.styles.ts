37c7fcb8625b352d2487bed1985d3c37f36d81b2
catrt <rt990122@gmail.com>
2024-06-29 14:46:24 +0900

    Design: 회원가입 제출폼 media query 수정

--- src/components/SignupForm/SignupForm.styles.ts
+++ src/components/SignupForm/SignupForm.styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { ContainerStyled } from "../Container";
+import { device } from "@/styles/media";
 
 export const SignupFormContainer = styled(ContainerStyled)`
   padding-top: 4rem;
@@ -11,6 +12,11 @@ export const SignupFormStyled = styled.form`
   justify-content: center;
   align-items: center;
   gap: 1.6rem;
+
+  @media ${device.mobile} {
+    width: 100%;
+    max-width: 40rem;
+  }
 `;
 
 export const ToLoginText = styled.p`
