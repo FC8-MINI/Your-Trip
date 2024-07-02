84819db2da33a6060ad83032a2fcb619ba8590c3
minnug <parkminnug@gmail.com>
2024-07-02 14:00:24 +0900

    Fix: 시간 형식 추가(년, 월, 일), 날짜 계산 추가(숙박 일)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,12 +3,18 @@ export const SECOND_TO_MILLISECOND = 1000;
 export const MINUTE_TO_SECOND = 60;
 export const HOUR_TO_MINUTE = 60;
 
-export function formatTime(date: string, format: "HH:MM" | "HH:MM:SS" | "YYYY:MM:DD HH:MM:SS" = "HH:MM") {
+export function formatTime(
+  date: string,
+  format: "HH:MM" | "HH:MM:SS" | "YYYY:MM:DD HH:MM:SS" | "YYYY년 MM월 DD일 HH:MM" = "HH:MM",
+) {
   const dateString = date.split("T")[0];
   const timeString = date.split("T")[1].split(":");
   const hourString = timeString[0];
   const minuteString = timeString[1];
   const secondString = timeString[2];
+  const year = dateString.split("-")[0];
+  const month = dateString.split("-")[1];
+  const day = dateString.split("-")[2];
 
   switch (format) {
     case "HH:MM":
@@ -17,6 +23,8 @@ export function formatTime(date: string, format: "HH:MM" | "HH:MM:SS" | "YYYY:MM
       return hourString + ":" + minuteString + ":" + secondString;
     case "YYYY:MM:DD HH:MM:SS":
       return dateString + " " + hourString + ":" + minuteString + ":" + secondString;
+    case "YYYY년 MM월 DD일 HH:MM":
+      return `${year}년 ${month}월 ${day}일 ${hourString}:${minuteString}`;
   }
 }
 
@@ -31,5 +39,18 @@ export const getCurrentKSTDateTimeLocal = (): string => {
   const hours = String(kstTime.getUTCHours()).padStart(2, "0");
   const minutes = String(kstTime.getUTCMinutes()).padStart(2, "0");
 
-  return `${year}-${month}-${day}T${hours}:${minutes}`;
+  const formattedTime = `${year}-${month}-${day}T${hours}:${minutes}`;
+
+  return formatTime(formattedTime, "YYYY년 MM월 DD일 HH:MM");
+};
+
+export const calculateNight = (checkIn: string, checkOut: string): number => {
+  const checkInDate = new Date(checkIn);
+  const checkOutDate = new Date(checkOut);
+
+  const timeDifference = checkOutDate.getTime() - checkInDate.getTime();
+
+  const night = timeDifference / (SECOND_TO_MILLISECOND * MINUTE_TO_SECOND * HOUR_TO_MINUTE * 24);
+
+  return Math.round(night);
 };
