44d760b00453aebfdf120cf7245a74c9e6e2136d
catrt <rt990122@gmail.com>
2024-07-02 18:52:31 +0900

    Fix: cache 오류 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { getAccommodationList } from "@/apis/accommodationList/getAccommodationList";
 import AccomodationEmpty from "@/components/Accomodation/AccomodationEmpty";
 import AccomodationList from "@/components/Accomodation/AccomodationList";
