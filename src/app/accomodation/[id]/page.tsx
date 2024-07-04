6707b0bbf16d349e9cd000c02cbca03af39b1f85
minnug <parkminnug@gmail.com>
2024-07-05 00:44:54 +0900

    Fix: 예약상세조회 API 관련 옵션 수정

--- src/app/accomodation/[id]/page.tsx
+++ src/app/accomodation/[id]/page.tsx
@@ -1,12 +1,14 @@
 import AccomodationDetail from "@/components/Accomodation/AccomodationDetail";
 
 const Accomodation = async ({ params: { id } }: { params: { id: string } }) => {
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/accommodation/${id}`;
+  const url = `https://api.miniteam2.store/api/accommodation/${id}`;
   const response = await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
+    cache: "no-store",
   });
   const { body: data } = await response.json();
 
