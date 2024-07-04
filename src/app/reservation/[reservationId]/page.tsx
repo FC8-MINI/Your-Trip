aaa61af3b2b005a98e12c2e34259eff09b72dddc
minnug <parkminnug@gmail.com>
2024-07-04 17:38:26 +0900

    Fix: 예약조회 클라이언트에서 API 요청으로 수정

--- src/app/reservation/[reservationId]/page.tsx
+++ src/app/reservation/[reservationId]/page.tsx
@@ -1,19 +1,42 @@
-import ReservationDetail from "@/components/Reservation/ReservationDetail";
+"use client";
 
-const ReservationDatailPage = async ({ params: { reservationId } }: { params: { reservationId: string } }) => {
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/reservation/${reservationId}`;
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const { body: data } = await response.json();
+import { useEffect, useState } from "react";
+import ReservationDetail, { ReservationDetailProps } from "@/components/Reservation/ReservationDetail";
+
+const ReservationDetailPage = ({ params: { reservationId } }: { params: { reservationId: string } }) => {
+  const [data, setData] = useState<ReservationDetailProps>();
+
+  useEffect(() => {
+    const fetchReservationDetail = async () => {
+      try {
+        const url = ` https://api.miniteam2.store/api/reservation/detail/${reservationId}`;
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const result = await response.json();
+        setData(result.body);
+      } catch (error) {
+        console.error("Error fetching reservation detail:", error);
+      }
+    };
+
+    fetchReservationDetail().catch((error) => {
+      console.error("Error in fetchReservationDetail:", error);
+    });
+  }, [reservationId]);
+
+  if (!data) {
+    return <div>Error loading data</div>;
+  }
 
   return (
     <ReservationDetail
       accomodationName={data.accomodationName}
-      userName={data.userName}
+      memberName={data.memberName}
       roomName={data.roomName}
       checkIn={data.checkIn}
       checkOut={data.checkOut}
@@ -22,8 +45,9 @@ const ReservationDatailPage = async ({ params: { reservationId } }: { params: { 
       parkingAvailable={data.parkingAvailable}
       cookingAvailable={data.cookingAvailable}
       roomPrice={data.roomPrice}
+      roomImageUrls={data.roomImageUrls}
     />
   );
 };
 
-export default ReservationDatailPage;
+export default ReservationDetailPage;
--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Pagination from "@/components/Pagination";
 import Reservation from "@/components/Reservation";
 import ReservationEmpty from "@/components/Reservation/ReservationEmpty";
@@ -8,22 +11,40 @@ interface ReservationProps {
   };
 }
 
-const ReservationPage = async ({ searchParams: { page } }: ReservationProps) => {
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/reservation`;
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const { body } = await response.json();
+const ReservationPage = ({ searchParams: { page } }: ReservationProps) => {
+  const [reservationItems, setReservationItems] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    const fetchReservation = async () => {
+      try {
+        const url = `https://api.miniteam2.store/api/reservation`;
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const { body } = await response.json();
+
+        setReservationItems(body.content);
+        setTotalPages(body.totalPages);
+      } catch (error) {
+        console.error("Error fetching reservations:", error);
+      }
+    };
+
+    fetchReservation().catch((error) => {
+      console.error("Error in fetchReservations:", error);
+    });
+  }, [page]);
 
   return (
     <>
-      {body.content.length > 0 ? (
+      {reservationItems.length > 0 ? (
         <>
-          <Reservation reservationItems={body.content} />
-          <Pagination maxPage={body.totalPages} nowPage={Number(page) || 1} />
+          <Reservation reservationItems={reservationItems} />
+          <Pagination maxPage={totalPages} nowPage={Number(page) || 1} />
         </>
       ) : (
         <ReservationEmpty />
