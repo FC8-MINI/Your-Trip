59ff8d28d6b06faf4bdde4b5513f7acf11f4b4be
minnug <parkminnug@gmail.com>
2024-07-05 12:41:11 +0900

    Fix: 드롭다운메뉴 링크 이름 수정

--- src/components/DropdownMenu/DropdownMenu.constants.ts
+++ src/components/DropdownMenu/DropdownMenu.constants.ts
@@ -1,6 +1,6 @@
 export const DROPDOWN_MENU_LINKS = [
   { name: "장바구니", href: "/cart" },
   { name: "예약 내역", href: "/reservation" },
-  { name: "내 찜 목록", href: "/like" },
+  { name: "내 찜 목록", href: "/likes" },
   { name: "닉네임 변경", href: "/edit" },
 ];
